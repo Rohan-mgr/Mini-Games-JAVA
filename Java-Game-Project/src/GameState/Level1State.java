240ca9ce95c4818fd6d5ec3eed7ca653255a9381
Rohan-mgr <rohan.magar.415@gmail.com>
2024-02-07 14:20:08 +0545

    added arrow key up for flying

--- Java-Game-Project/src/GameState/Level1State.java
+++ Java-Game-Project/src/GameState/Level1State.java
@@ -119,9 +119,9 @@ public class Level1State extends GameState {
         hud.draw(g);
     };
     public void keyPressed(int k){
-        if(k == KeyEvent.VK_LEFT) player.setLeft(true);
-        if(k == KeyEvent.VK_RIGHT) player.setRight(true);
-        if(k == KeyEvent.VK_UP) player.setUp(true);
+        if(k == KeyEvent.VK_LEFT || k == KeyEvent.VK_A) player.setLeft(true);
+        if(k == KeyEvent.VK_RIGHT || k == KeyEvent.VK_D) player.setRight(true);
+        if(k == KeyEvent.VK_UP) player.setJumping(true);
         if(k == KeyEvent.VK_DOWN) player.setDown(true);
         if(k == KeyEvent.VK_W) player.setJumping(true);
         if(k == KeyEvent.VK_E) player.setGliding(true);
@@ -129,9 +129,9 @@ public class Level1State extends GameState {
         if(k == KeyEvent.VK_F) player.setFiring();
     };
     public void keyReleased(int k) {
-        if(k == KeyEvent.VK_LEFT) player.setLeft(false);
-        if(k == KeyEvent.VK_RIGHT) player.setRight(false);
-        if(k == KeyEvent.VK_UP) player.setUp(false);
+        if(k == KeyEvent.VK_LEFT || k == KeyEvent.VK_A) player.setLeft(false);
+        if(k == KeyEvent.VK_RIGHT || k == KeyEvent.VK_D) player.setRight(false);
+        if(k == KeyEvent.VK_UP) player.setJumping(false);
         if(k == KeyEvent.VK_DOWN) player.setDown(false);
         if(k == KeyEvent.VK_W) player.setJumping(false);
         if(k == KeyEvent.VK_E) player.setGliding(false);
